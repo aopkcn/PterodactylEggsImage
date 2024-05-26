af830d6b3b6ff96a2a6a74fa971bbe81c0c6b21f
蝼蚁梦 <45751694+aopkcn@users.noreply.github.com>
2024-05-26 10:35:54 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# 添加设置源的命令
+cat > /etc/apt/sources.list << EOF
+deb https://mirrors.cloud.tencent.com/debian/ bullseye main contrib non-free
+EOF
+
+# 构建镜像
+docker build -t games:valheim .
+
+
+# GitHub 用户名和镜像名称
+GHCR_USERNAME="aopkcn"
+REPOSITORY_NAME="games"
+IMAGE_TAG="valheim"
+
+# 登录到 GitHub Container Registry
+echo "key" | docker login ghcr.io -u $GHCR_USERNAME --password-stdin
+
+# 重新打标签
+docker tag $REPOSITORY_NAME:$IMAGE_TAG ghcr.io/$GHCR_USERNAME/$REPOSITORY_NAME:$IMAGE_TAG
+
+# 推送镜像
+docker push ghcr.io/$GHCR_USERNAME/$REPOSITORY_NAME:$IMAGE_TAG
+
+# 检查推送状态
+if [ $? -eq 0 ]; then
+  echo "镜像推送成功！"
+else
+  echo "镜像推送失败！"
+fi
